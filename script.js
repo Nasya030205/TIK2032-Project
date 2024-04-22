b259b24ce5940cfb54bd398dbd296846dc4652cd
nasya03 <nasyaacha99@gmail.com>
2024-04-22 09:16:06 +0700

    second commit

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-// galery.js
 document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll(".gallery-img");
-    const lightbox = document.querySelector(".lightbox");
-    const lightboxImg = document.querySelector(".lightbox-img");
-    const closeBtn = document.querySelector(".close");
+    // Select elemen-elemen yang dibutuhkan
+    const logoTitle = document.querySelector(".logo-title");
+    const containerImg = document.querySelector(".container img");
 
-    images.forEach(img => {
-        img.addEventListener("click", function() {
-            const src = this.getAttribute("src");
-            lightboxImg.setAttribute("src", src);
-            lightbox.style.display = "block";
-        });
+    // Tambahkan event listener untuk efek mouseover pada logo
+    logoTitle.addEventListener("mouseover", function() {
+        this.style.color = "#FF6A3D";
+        this.style.transform = "rotate(2deg)";
     });
 
-    closeBtn.addEventListener("click", function() {
-        lightbox.style.display = "none";
+    // Tambahkan event listener untuk efek mouseout pada logo
+    logoTitle.addEventListener("mouseout", function() {
+        this.style.color = "#eaeaea";
+        this.style.transform = "rotate(0deg)";
+    });
+
+    // Tambahkan event listener untuk efek zoom pada gambar saat dihover
+    containerImg.addEventListener("mouseover", function() {
+        this.style.transform = "scale(1.1)";
+    });
+
+    // Tambahkan event listener untuk mengembalikan ukuran normal gambar saat mouse keluar
+    containerImg.addEventListener("mouseout", function() {
+        this.style.transform = "scale(1)";
     });
 });
